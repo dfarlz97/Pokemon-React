c1c7b8172fa418a59625cf5182ef3fc080adb788
+ <+>
2023-04-13 10:53:25 -0400

    add

--- components-and-props/src/Pokemon.jsx
+++ components-and-props/src/Pokemon.jsx
@@ -6,31 +6,16 @@ function Pokemon({ pokemon, title }) {
   const renderPokemonDetail = pokemon.map((poke) => {
     return (
       <PokemonDetail
+        key={poke.name}
         name={poke.name}
-        type={poke.type}
         image={poke.image}
-        region={poke.region}
-        abilities={poke.abilities.ability}
       ></PokemonDetail>
     );
   });
-  //   [{}, {}, {}];
-
-  // const {pokemon, title} = props
   return (
     <div>
-      <h1
-        style={{
-          fontFamily: "montserrat",
-          color: "Red",
-        }}
-      >
-        {title}
-      </h1>
-
-      {renderPokemonDetail}
-      {/* <Mewtwo mewtwo={pokemon} />
-      <IronValiant /> */}
+      <h1>{title}</h1>
+      <div className="pokemon-detail">{renderPokemonDetail}</div>
     </div>
   );
 }
--- components-and-props/src/PokemonDetail.jsx
+++ components-and-props/src/PokemonDetail.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 
-function PokemonDetail({name, image, type, region, abilities}) {
+function PokemonDetail({ name, image }) {
+  console.log(name, image);
   return (
     <div>
-        <img src={image} style={{
-        width: "25%",
-        }}></img> 
-            <p>{name}</p>
-            <p>Type: {type}</p>
-            <p>Region: {region}</p>
-            <p>Ability: {abilities}</p>     
+      <img height={250} src={image}></img>
+      <p>{name}</p>
     </div>
   );
 }
+
 export default PokemonDetail;
